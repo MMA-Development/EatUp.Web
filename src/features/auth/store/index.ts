9d907160ecfb4f3fe744d247ed21ec78bc24daba
Matteo <matteovanggard@gmail.com>
2025-04-30 12:52:25 +0200

    feat(auth): enhance token management with access and refresh tokens, add logout action

--- src/features/auth/store/index.ts
+++ src/features/auth/store/index.ts
@@ -1,10 +1,15 @@
 import { createSlice, PayloadAction } from '@reduxjs/toolkit'
 import { RootState } from '@store/types.ts'
 
+interface Token {
+  accessToken: string
+  refreshToken: string
+}
+
 export interface Auth {
   isAuthenticated: boolean
   user: string | null
-  token: string | null
+  token: Token | null
 }
 
 export const initialAuthState: Auth = {
@@ -17,17 +22,25 @@ const authSlice = createSlice({
   name: 'auth',
   initialState: initialAuthState,
   reducers: {
-    setToken(state, action: PayloadAction<string | null>) {
+    setToken(state, action: PayloadAction<Token | null>) {
       state.token = action.payload
       state.isAuthenticated = action.payload !== null
     },
+    setAccessToken(state, action: PayloadAction<string>) {
+      ;(state.token as Token).accessToken = action.payload
+    },
     setUser(state, action: PayloadAction<string | null>) {
       state.user = action.payload
+    },
+    logout(state) {
+      state.token = null
+      state.user = null
+      state.isAuthenticated = false
     }
   }
 })
 
-export const { setToken, setUser } = authSlice.actions
+export const { setToken, setAccessToken, setUser, logout } = authSlice.actions
 
 export const selectToken = (state: RootState) => state.auth.token
 export const selectUser = (state: RootState) => state.auth.user
