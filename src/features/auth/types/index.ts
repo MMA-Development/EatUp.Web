63091d61997a981c1dff73b097683dc878511a04
Matteo <matteovanggard@gmail.com>
2025-04-30 12:54:24 +0200

    feat(auth): update auth schemas for improved flexibility
    - Reduced minimum password length to 4.
    - Added refreshToken to LoginResponse schema.

--- src/features/auth/types/index.ts
+++ src/features/auth/types/index.ts
@@ -2,13 +2,14 @@ import { z } from 'zod'
 
 export const LoginPayloadSchema = z.object({
   username: z.string().min(3),
-  password: z.string().min(7)
+  password: z.string().min(4)
 })
 
 export type LoginPayload = z.infer<typeof LoginPayloadSchema>
 
 export const LoginResponseSchema = z.object({
-  token: z.string()
+  accessToken: z.string(),
+  refreshToken: z.string()
 })
 
 export type LoginResponse = z.infer<typeof LoginResponseSchema>
