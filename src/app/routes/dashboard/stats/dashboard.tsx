8ab23f570cea50b8f4b9108bd9ebff6445b872d1
Matteo <matteovanggard@gmail.com>
2025-05-13 14:26:29 +0200

    `refactor(dashboard): update layout to use SimpleGrid and enhance orders sparkline display`

--- src/app/routes/dashboard/stats/dashboard.tsx
+++ src/app/routes/dashboard/stats/dashboard.tsx
@@ -1,10 +1,15 @@
-import { Flex } from '@chakra-ui/react'
+import { Flex, SimpleGrid } from '@chakra-ui/react'
 import { OrdersTodaySparkline } from '@features/orders/components/orders-today-sparkline.tsx'
 
 export function Dashboard() {
   return (
     <Flex direction={'column'} gap={4}>
-      <OrdersTodaySparkline />
+      <SimpleGrid minChildWidth="sm" gap="40px">
+        <OrdersTodaySparkline />
+        <OrdersTodaySparkline />
+        <OrdersTodaySparkline />
+        <OrdersTodaySparkline />
+      </SimpleGrid>
     </Flex>
   )
 }
