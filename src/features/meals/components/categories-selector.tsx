ede5590c8a29f2e76866f59dc14fe3baf77b2132
Matteo <matteovanggard@gmail.com>
2025-05-28 10:12:11 +0200

    feat(meals): add translations to CategoriesSelector placeholder

--- src/features/meals/components/categories-selector.tsx
+++ src/features/meals/components/categories-selector.tsx
@@ -2,8 +2,11 @@ import { useGetCategoriesQuery } from '@features/meals/api/get-categories.ts'
 import { useMemo } from 'react'
 import { createListCollection, Portal, Select, Spinner } from '@chakra-ui/react'
 import { Category } from '@features/meals/types'
+import { useTranslation } from 'react-i18next'
 
 export function CategoriesSelector({ ...props }: Omit<Select.RootProps<Category>, 'collection'>) {
+  const { t } = useTranslation('meals')
+
   const { data, isLoading } = useGetCategoriesQuery()
 
   const collection = useMemo(() => {
@@ -19,7 +22,7 @@ export function CategoriesSelector({ ...props }: Omit<Select.RootProps<Category>
       <Select.HiddenSelect />
       <Select.Control>
         <Select.Trigger>
-          <Select.ValueText placeholder="select.category" />
+          <Select.ValueText placeholder={t('select.category')} />
         </Select.Trigger>
         <Select.IndicatorGroup>
           {isLoading && <Spinner size="xs" borderWidth="1.5px" color="fg.muted" />}
